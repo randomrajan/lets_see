3ad7c00da45f0d0890fe18b79b0c6dd83322e383
theflaky <rajan2499@gmail.com>
2020-05-17 00:04:48 +0530

    commit

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Person from './Person/Person'
 
-const persons = (props) =>
-{
-    return(
+const persons = (props) =>  persons.map((person, index) => {
+        return <Person
+          click={() => this.deletePersonHandler(index)}
+          name={person.name} 
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.nameChangedHandler(event, person.id)} />
+      } );
 
-    );
-}
